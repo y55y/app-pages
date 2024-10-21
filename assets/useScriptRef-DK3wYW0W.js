dc4ce84c94a221d58892525eddd367706f20a34b
y55y <y55y@users.noreply.github.com>
2024-10-21 16:20:09 +0000

    deploy: y55y/react-app-frontend@09ffc3a854529a29728ea0525d656cbf6155dde4

--- assets/useScriptRef-DK3wYW0W.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index-C0c3XiV2.js";const s=()=>{const e=r.useRef(!0);return r.useEffect(()=>{e.current=!1},[]),e};export{s as u};
---
+++ assets/useScriptRef-KWRootDa.js
@@ -0,0 +1,1 @@
+import{r}from"./index-DoHa3M9S.js";const s=()=>{const e=r.useRef(!0);return r.useEffect(()=>{e.current=!1},[]),e};export{s as u};
