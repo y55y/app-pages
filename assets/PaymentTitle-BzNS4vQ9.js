adef018a0973a921d4b4b57cb9a8648f55141f34
y55y <y55y@users.noreply.github.com>
2025-01-02 17:27:23 +0000

    deploy: y55y/react-app-frontend@4eb32348049eedc840078dafe956dfa55dae937f

---
+++ assets/PaymentTitle-BzNS4vQ9.js
@@ -0,0 +1,1 @@
+import{P as r}from"./index-C9hy9DEl.js";r.number.isRequired;
--- assets/userinfo-Bx4pVl_2.js
+++
@@ -1,1 +0,0 @@
-import{a5 as a}from"./index-Cy6-NqMz.js";const s=async()=>{try{return(await a.get("/api/locations")).data}catch(o){throw console.error("Error getting locations data:",o.response?o.response.data:o.message),o}};export{s as g};
