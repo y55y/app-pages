0140c5705d4fd9b15a86e158b3ed488af42a6a88
y55y <y55y@users.noreply.github.com>
2025-02-23 08:26:59 +0000

    deploy: y55y/react-app-frontend@9186b9344d36a1fc2160d731aa8f667f99b6f31f

---
+++ assets/OpenInNew-vxh-r961.js
@@ -0,0 +1,1 @@
+import{e as r,i as t,j as a}from"./index-Dq6sVYkr.js";var e={},u=t;Object.defineProperty(e,"__esModule",{value:!0});var o=e.default=void 0,v=u(r()),i=a;o=e.default=(0,v.default)((0,i.jsx)("path",{d:"M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3z"}),"OpenInNew");export{o as d};
