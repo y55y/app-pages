697910eff82e4e7dab9ea2f00bd6d9d1a7c20ebe
y55y <y55y@users.noreply.github.com>
2025-03-22 01:29:03 +0000

    deploy: y55y/react-app-frontend@5969cfa4d437499105668b7a269a6c5cac7c2090

---
+++ assets/IconX-Ci2obL0K.js
@@ -0,0 +1,1 @@
+import{k as a}from"./index-CvGIXqP7.js";var c=a("check","IconCheck",[["path",{d:"M5 12l5 5l10 -10",key:"svg-0"}]]),o=a("x","IconX",[["path",{d:"M18 6l-12 12",key:"svg-0"}],["path",{d:"M6 6l12 12",key:"svg-1"}]]);export{c as I,o as a};
