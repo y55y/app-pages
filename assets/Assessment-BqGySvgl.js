f78130d08678e53559fb350bf9fa821a303ef0c1
y55y <y55y@users.noreply.github.com>
2025-03-22 01:43:32 +0000

    deploy: y55y/react-app-frontend@75976f757eb41c6d6a38a75bb11404cc5b11bf3a

--- assets/Assessment-BqGySvgl.js
+++
@@ -1,1 +0,0 @@
-import{r,i as t,j as a}from"./index-CvGIXqP7.js";var e={},s=t;Object.defineProperty(e,"__esModule",{value:!0});var u=e.default=void 0,o=s(r()),v=a;u=e.default=(0,o.default)((0,v.jsx)("path",{d:"M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2M9 17H7v-7h2zm4 0h-2V7h2zm4 0h-2v-4h2z"}),"Assessment");export{u as d};
