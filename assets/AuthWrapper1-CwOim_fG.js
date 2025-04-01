ca0e6b545e43f4fbe63f435ef83c9c557e5b54ce
y55y <y55y@users.noreply.github.com>
2025-04-01 13:15:18 +0000

    deploy: y55y/react-app-frontend@4f088b187397f832dd02a9d8ebb57a2af96ade0b

---
+++ assets/AuthWrapper1-CwOim_fG.js
@@ -0,0 +1,1 @@
+import{s as t,d as a}from"./index-BP_9qwSP.js";const r=t("div")(({theme:e})=>({backgroundColor:e.palette.mode===a.DARK?e.palette.background.default:e.palette.grey[100],minHeight:"100vh"}));export{r as A};
