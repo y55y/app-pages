d63044e263c33f7d82dbaa3f982efe7c6591c073
y55y <y55y@users.noreply.github.com>
2025-05-15 10:54:36 +0000

    deploy: y55y/react-app-frontend@9a1e0789eb779dc966927d719e9d710cbb1b09df

---
+++ assets/Download-BQp0giJk.js
@@ -0,0 +1,1 @@
+import{i as a,k as r,j as t}from"./index-o6XZWwFu.js";var e={},o=r;Object.defineProperty(e,"__esModule",{value:!0});var u=e.default=void 0,d=o(a()),i=t;u=e.default=(0,d.default)((0,i.jsx)("path",{d:"M5 20h14v-2H5zM19 9h-4V3H9v6H5l7 7z"}),"Download");export{u as d};
