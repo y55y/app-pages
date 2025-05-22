6e3ccd551352dfc5277215a8d1213017d99a3a2b
y55y <y55y@users.noreply.github.com>
2025-05-22 04:33:07 +0000

    deploy: y55y/react-app-frontend@448a8b3c152cf90578723ca40b6ab8722138bd21

---
+++ assets/CalendarMonth-KEn3Yrif.js
@@ -0,0 +1,1 @@
+import{i as h,j as a}from"./index-UgNdx5nq.js";const o=h(a.jsx("path",{d:"M19 4h-1V2h-2v2H8V2H6v2H5c-1.11 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2m0 16H5V10h14zM9 14H7v-2h2zm4 0h-2v-2h2zm4 0h-2v-2h2zm-8 4H7v-2h2zm4 0h-2v-2h2zm4 0h-2v-2h2z"}),"CalendarMonth");export{o as C};
