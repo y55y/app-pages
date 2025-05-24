f4fc61aea9fbc99d9f3a4842e12e2fc1fe59ff3c
y55y <y55y@users.noreply.github.com>
2025-05-24 19:05:22 +0000

    deploy: y55y/react-app-frontend@dd2cc395719ac8627fea17c33210cbd7305c360b

--- assets/AuthCardWrapper-B7IKb2Ud.js
+++
@@ -1,1 +0,0 @@
-import{P as a,j as s,M as e,v as o}from"./index-BKiRBGsf.js";const p=({children:x,...r})=>s.jsx(e,{sx:{maxWidth:{xs:400,lg:475},margin:{xs:2.5,md:3},"& > *":{flexGrow:1,flexBasis:"50%"}},content:!1,...r,children:s.jsx(o,{sx:{p:{xs:2,sm:3,xl:5}},children:x})});p.propTypes={children:a.node};export{p as A};
