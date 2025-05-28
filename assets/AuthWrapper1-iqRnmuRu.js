cfffb301c3321e1197e3293797c79f214f738ff9
y55y <y55y@users.noreply.github.com>
2025-05-28 08:12:36 +0000

    deploy: y55y/react-app-frontend@a599de071b5abea4cdfb0d3c936c8940123db741

---
+++ assets/AuthWrapper1-iqRnmuRu.js
@@ -0,0 +1,1 @@
+import{s as t,p as a}from"./index-SVJqW2-d.js";const r=t("div")(({theme:e})=>({backgroundColor:e.palette.mode===a.DARK?e.palette.background.default:e.palette.grey[100],minHeight:"100vh"}));export{r as A};
