feb27f41555aa90ea4b3ce27cc39e7c005270743
y55y <y55y@users.noreply.github.com>
2025-05-28 16:59:48 +0000

    deploy: y55y/react-app-frontend@e94e8631751e1a10b6e4679a7f039d536a108b0e

--- assets/Assessment-BXEDZk9k.js
+++
@@ -1,1 +0,0 @@
-import{i as s,j as t}from"./index-BNW2uOrr.js";const e=s(t.jsx("path",{d:"M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2M9 17H7v-7h2zm4 0h-2V7h2zm4 0h-2v-4h2z"}),"Assessment");export{e as A};
