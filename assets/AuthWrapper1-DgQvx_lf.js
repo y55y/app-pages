45c5f1ed569a1692944286903f7075492f1675b3
y55y <y55y@users.noreply.github.com>
2025-05-29 14:48:55 +0000

    deploy: y55y/react-app-frontend@e2a5f2955d5d2dd1a09a1eb04e6dcedcdf7b4aad

--- assets/AuthWrapper1-DgQvx_lf.js
+++
@@ -1,1 +0,0 @@
-import{s as t,p as a}from"./index-DxQVm8F8.js";const r=t("div")(({theme:e})=>({backgroundColor:e.palette.mode===a.DARK?e.palette.background.default:e.palette.grey[100],minHeight:"100vh"}));export{r as A};
