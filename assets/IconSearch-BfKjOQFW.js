bd063e575e05446fdb0f2dba5cdd961986a2b63b
y55y <y55y@users.noreply.github.com>
2025-06-16 14:59:40 +0000

    deploy: y55y/react-app-frontend@42682e6cbb626eebe58ed7161f614dba40abb1df

--- assets/IconSearch-BfKjOQFW.js
+++
@@ -1,1 +0,0 @@
-import{Q as a}from"./index-B8Od8jCQ.js";var r=a("search","IconSearch",[["path",{d:"M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0",key:"svg-0"}],["path",{d:"M21 21l-6 -6",key:"svg-1"}]]);export{r as I};
