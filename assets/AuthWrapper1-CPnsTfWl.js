496edcac3bbdaad5a3fd20565187ad40a0ac9d7d
y55y <y55y@users.noreply.github.com>
2025-06-17 16:00:42 +0000

    deploy: y55y/react-app-frontend@8249f5f8552a46500ee03c7e9dde8ea76de930ff

---
+++ assets/AuthWrapper1-CPnsTfWl.js
@@ -0,0 +1,1 @@
+import{s as t,p as a}from"./index-Bgc4vok_.js";const r=t("div")(({theme:e})=>({backgroundColor:e.palette.mode===a.DARK?e.palette.background.default:e.palette.grey[100],minHeight:"100vh"}));export{r as A};
