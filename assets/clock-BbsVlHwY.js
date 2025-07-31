241ba79a3ae7e5b8844ab550eb79c95d82b8a621
y55y <y55y@users.noreply.github.com>
2025-07-31 17:25:09 +0000

    deploy: y55y/react-app-frontend@daf0621c1a7c9eef0983bc817169b8be27c58a95

---
+++ assets/clock-BbsVlHwY.js
@@ -0,0 +1,11 @@
+import{b as c}from"./index-WgOv4Jdi.js";/**
+ * @license lucide-react v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M8 2v4",key:"1cmpym"}],["path",{d:"M16 2v4",key:"4m81vk"}],["rect",{width:"18",height:"18",x:"3",y:"4",rx:"2",key:"1hopcy"}],["path",{d:"M3 10h18",key:"8toen8"}]],a=c("calendar",e);/**
+ * @license lucide-react v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M12 6v6l4 2",key:"mmk7yg"}],["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}]],y=c("clock",o);export{y as C,a};
