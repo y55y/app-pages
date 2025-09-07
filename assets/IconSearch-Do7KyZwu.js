ef3866bb7b63ea19b344ad431e4d1459a5ba55e3
y55y <y55y@users.noreply.github.com>
2025-09-07 09:08:57 +0000

    deploy: y55y/react-app-frontend@cacbfd508d89d1fd85d6390a4cd80753d6f2b9cb

--- assets/IconSearch-Do7KyZwu.js
+++
@@ -1,6 +0,0 @@
-import{aG as a}from"./index-BqHIMAGu.js";/**
- * @license @tabler/icons-react v3.34.1 - MIT
- *
- * This source code is licensed under the MIT license.
- * See the LICENSE file in the root directory of this source tree.
- */const e=[["path",{d:"M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0",key:"svg-0"}],["path",{d:"M21 21l-6 -6",key:"svg-1"}]],t=a("outline","search","Search",e);export{t as I};
