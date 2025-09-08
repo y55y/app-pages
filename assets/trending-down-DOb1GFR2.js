232e3781e97871fd99d8e09f02a059eaeedf0cd4
y55y <y55y@users.noreply.github.com>
2025-09-08 06:57:48 +0000

    deploy: y55y/react-app-frontend@97c09326f4e6e328ff7175e4564d2351a222d686

--- assets/trending-down-DOb1GFR2.js
+++
@@ -1,6 +0,0 @@
-import{e as n}from"./index-B4hwFf5w.js";/**
- * @license lucide-react v0.539.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const e=[["path",{d:"M16 17h6v-6",key:"t6n2it"}],["path",{d:"m22 17-8.5-8.5-5 5L2 7",key:"x473p"}]],t=n("trending-down",e);export{t as T};
