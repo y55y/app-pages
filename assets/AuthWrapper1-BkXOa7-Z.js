4fe1e64a77b943776f5d586e148a0950835d47d0
y55y <y55y@users.noreply.github.com>
2025-09-28 14:00:16 +0000

    deploy: y55y/react-app-frontend@e834c2741e802fc13da99eeaacadcee85bbe3cd1

--- assets/AuthWrapper1-BkXOa7-Z.js
+++
@@ -1,1 +0,0 @@
-import{T as o}from"./index-BLp36HJn.js";import{K as t}from"./vendor-BgcwOeBV.js";const p=t("div")(({theme:e})=>({backgroundColor:e.palette.mode===o.DARK?e.palette.background.default:e.palette.grey[100],minHeight:"100vh"}));export{p as A};
