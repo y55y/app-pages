139ce1c6ab156f7c49268181cac2826fca80e986
y55y <y55y@users.noreply.github.com>
2025-10-03 11:01:14 +0000

    deploy: y55y/react-app-frontend@71f22dd0e52eae65fe5b5f5e02e7fd3a6c57aef4

--- assets/AuthWrapper1-CcoNvKNQ.js
+++
@@ -1,1 +0,0 @@
-import{T as o}from"./index-C8BZjYI9.js";import{K as t}from"./vendor-BS8--Smi.js";const p=t("div")(({theme:e})=>({backgroundColor:e.palette.mode===o.DARK?e.palette.background.default:e.palette.grey[100],minHeight:"100vh"}));export{p as A};
