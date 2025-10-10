087a0dae6eecaf830ac73c027a01bacdb53a0b6f
y55y <y55y@users.noreply.github.com>
2025-10-10 21:15:26 +0000

    deploy: y55y/react-app-frontend@a90714e18dda8580afe55b3a1190380f1962e8e4

---
+++ assets/IconEye-DAASt4pi.js
@@ -0,0 +1,6 @@
+import{j as e}from"./index-CaqMclU3.js";/**
+ * @license @tabler/icons-react v3.34.1 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0",key:"svg-0"}],["path",{d:"M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6",key:"svg-1"}]],t=e("outline","eye","Eye",o);export{t as I};
