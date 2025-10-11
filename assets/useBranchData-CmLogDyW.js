8877489e159c129f6ef27e1e0b6f3ee292535f25
y55y <y55y@users.noreply.github.com>
2025-10-11 08:36:03 +0000

    deploy: y55y/react-app-frontend@2eee9b7ae25dac607e33a099cccaa080223d51f8

---
+++ assets/useBranchData-CmLogDyW.js
@@ -0,0 +1,1 @@
+import{e as o}from"./index-CHrT17_s.js";const c=()=>{const{selectedBranch:a,branches:n,changeBranch:r,isLoading:s,refreshBranches:h}=o();return{selectedBranch:a,branches:n,changeBranch:r,refreshBranches:h,isLoading:s,branchId:(a==null?void 0:a.BranchID)||null,Name:(a==null?void 0:a.Name)||null}};export{c as u};
