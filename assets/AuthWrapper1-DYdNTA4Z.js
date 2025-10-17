5a58e53ff240152bf3dec3db446597ba28fd9267
y55y <y55y@users.noreply.github.com>
2025-10-17 18:45:36 +0000

    deploy: y55y/react-app-frontend@cf5152d051d9adfae65e8749f82496d3554a1030

---
+++ assets/AuthWrapper1-DYdNTA4Z.js
@@ -0,0 +1,1 @@
+import{T as o}from"./index-Dht9EbkF.js";import{K as t}from"./vendor-h5bvcGcX.js";const p=t("div")(({theme:e})=>({backgroundColor:e.palette.mode===o.DARK?e.palette.background.default:e.palette.grey[100],minHeight:"100vh"}));export{p as A};
