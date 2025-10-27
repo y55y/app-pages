f7e5eccae507c150108552e6d9e74fd6655a3908
y55y <y55y@users.noreply.github.com>
2025-10-27 16:55:59 +0000

    deploy: y55y/react-app-frontend@aea740cf2623491485501d2e2e16dec46620e48a

---
+++ assets/IconSearch-BMxy62zX.js
@@ -0,0 +1,11 @@
+import{k as a}from"./index-CrDu8A-8.js";/**
+ * @license @tabler/icons-react v3.35.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1",key:"svg-0"}],["path",{d:"M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z",key:"svg-1"}],["path",{d:"M16 5l3 3",key:"svg-2"}]],c=a("outline","edit","Edit",t);/**
+ * @license @tabler/icons-react v3.35.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0",key:"svg-0"}],["path",{d:"M21 21l-6 -6",key:"svg-1"}]],s=a("outline","search","Search",e);export{s as I,c as a};
