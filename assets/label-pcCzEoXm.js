23ff6374d433c55e4a5d0cd66e1882f27c11987e
y55y <y55y@users.noreply.github.com>
2025-11-06 17:35:25 +0000

    deploy: y55y/react-app-frontend@0532eeb59678b33f3c5adc58192348e5035aa52d

---
+++ assets/label-pcCzEoXm.js
@@ -0,0 +1,6 @@
+import{c as n,P as i}from"./card-DeVNwWR_.js";import{j as o}from"./vendor-B9baa07B.js";import{r as d}from"./react-BGTiXhcV.js";import{c as l}from"./utils-zVfqsIXo.js";/**
+ * @license lucide-react v0.543.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"M10 11v6",key:"nco0om"}],["path",{d:"M14 11v6",key:"outv1u"}],["path",{d:"M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6",key:"miytrc"}],["path",{d:"M3 6h18",key:"d0wm0j"}],["path",{d:"M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2",key:"e791ji"}]],y=n("trash-2",c);var u="Label",s=d.forwardRef((e,a)=>o.jsx(i.label,{...e,ref:a,onMouseDown:t=>{var r;t.target.closest("button, input, select, textarea")||((r=e.onMouseDown)==null||r.call(e,t),!t.defaultPrevented&&t.detail>1&&t.preventDefault())}}));s.displayName=u;var m=s;function v({className:e,...a}){return o.jsx(m,{"data-slot":"label",className:l("flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",e),...a})}export{v as L,y as T};
