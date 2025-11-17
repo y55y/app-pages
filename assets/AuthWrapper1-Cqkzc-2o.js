c416261046cda6dd9d9381c407c2d1369943b9a6
y55y <y55y@users.noreply.github.com>
2025-11-17 14:17:45 +0000

    deploy: y55y/react-app-frontend@59e8daec4c45fd4d7f2762875f1346a1df25de3c

---
+++ assets/AuthWrapper1-Cqkzc-2o.js
@@ -0,0 +1,1 @@
+import{T as o}from"./index-I1UfQG9-.js";import{R as t}from"./vendor-E9Dj-MSL.js";const p=t("div")(({theme:e})=>({backgroundColor:e.palette.mode===o.DARK?e.palette.background.default:e.palette.grey[100],minHeight:"100vh"}));export{p as A};
