a879fa2182ef508fc1e86bb282d57fb8f8076c0b
y55y <y55y@users.noreply.github.com>
2025-12-11 09:24:26 +0000

    deploy: y55y/react-app-frontend@cacee1d5717d7010fa363bcacf8491cca56b59d1

--- assets/tax_invoice_pro_ar_en-D0nELN9T.js
+++
@@ -1,1 +0,0 @@
-const t=""+new URL("tax_invoice_pro_ar_en-Dt3t4BMb.txt",import.meta.url).href;export{t as default};
---
+++ assets/tax_invoice_pro_ar_en-uLgJkqfX.js
@@ -0,0 +1,1 @@
+const e=""+new URL("tax_invoice_pro_ar_en-Dw6BqFPQ.txt",import.meta.url).href;export{e as default};
