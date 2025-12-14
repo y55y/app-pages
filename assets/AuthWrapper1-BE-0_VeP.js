efe792d6734cd2964e2a801d8dd22a74f2f87686
y55y <y55y@users.noreply.github.com>
2025-12-14 08:10:31 +0000

    deploy: y55y/react-app-frontend@78934b2a6a9e465a95b18c02401859b9b39f4926

--- assets/AuthWrapper1-BE-0_VeP.js
+++
@@ -1,1 +0,0 @@
-import{b as o}from"./index-CwoD5_r1.js";import{R as t}from"./vendor-BamrJDi8.js";const p=t("div")(({theme:e})=>({backgroundColor:e.palette.mode===o.DARK?e.palette.background.default:e.palette.grey[100],minHeight:"100vh"}));export{p as A};
