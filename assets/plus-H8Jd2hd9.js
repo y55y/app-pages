4a82360628ebd4f8ff579442b58d2ba9490c99cf
y55y <y55y@users.noreply.github.com>
2026-01-26 19:35:20 +0000

    deploy: y55y/react-app-frontend@afe9ac81d9442bdd4f6174633eb5a17f6e2eee2b

--- assets/plus-H8Jd2hd9.js
+++
@@ -1,6 +0,0 @@
-import{v as e}from"./index-Cgmuii4U.js";/**
- * @license lucide-react v0.543.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const o=[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]],t=e("plus",o);export{t as P};
