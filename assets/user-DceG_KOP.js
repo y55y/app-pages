d866ac5905f86fc336896d7cfe561e87745bbc85
y55y <y55y@users.noreply.github.com>
2026-02-08 10:26:00 +0000

    deploy: y55y/react-app-frontend@4511bc801c1d3c47b988c11a271bee7f71ec72f4

---
+++ assets/user-DceG_KOP.js
@@ -0,0 +1,11 @@
+import{f as e}from"./index-BXq8O7ya.js";/**
+ * @license lucide-react v0.543.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["line",{x1:"12",x2:"12",y1:"8",y2:"12",key:"1pkeuh"}],["line",{x1:"12",x2:"12.01",y1:"16",y2:"16",key:"4dfq90"}]],o=e("circle-alert",c);/**
+ * @license lucide-react v0.543.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=[["path",{d:"M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2",key:"975kel"}],["circle",{cx:"12",cy:"7",r:"4",key:"17ys0d"}]],i=e("user",r);export{o as C,i as U};
