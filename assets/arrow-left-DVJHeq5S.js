c1bc6ee75e950e25a6f9e2debf89b98e39a948c0
y55y <y55y@users.noreply.github.com>
2026-04-11 17:14:58 +0000

    deploy: y55y/react-app-frontend@6ff0109e1fd4a6066bcce951d782b5648909b438

--- assets/arrow-left-DVJHeq5S.js
+++
@@ -1,6 +0,0 @@
-import{f as o}from"./index-Buf1hquJ.js";/**
- * @license lucide-react v0.543.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const e=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],r=o("arrow-left",e);export{r as A};
---
+++ assets/skeleton-D5I-p76o.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-Ch2obJ5d.js";import{x as s}from"./index-BZhu6fxQ.js";function r({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
