2b76a11721879922497f5f5da4489016f149c2a4
y55y <y55y@users.noreply.github.com>
2026-04-11 10:11:24 +0000

    deploy: y55y/react-app-frontend@1a2b17de18beb9ecc0e5bba867696b5ecedc0e56

---
+++ assets/AuthWrapper1-DxL2Kbah.js
@@ -0,0 +1,1 @@
+import{b as o}from"./index-Bmrbjuga.js";import{R as t}from"./vendor-Ch2obJ5d.js";const p=t("div")(({theme:e})=>({backgroundColor:e.palette.mode===o.DARK?e.palette.background.default:e.palette.grey[100],minHeight:"100vh"}));export{p as A};
--- assets/AuthWrapper1-JUoMAovs.js
+++
@@ -1,1 +0,0 @@
-import{b as o}from"./index-DqwMp6EJ.js";import{R as t}from"./vendor-Ch2obJ5d.js";const p=t("div")(({theme:e})=>({backgroundColor:e.palette.mode===o.DARK?e.palette.background.default:e.palette.grey[100],minHeight:"100vh"}));export{p as A};
