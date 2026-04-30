b171bbcded569ea5d8c9874fdfdf47c16a301e11
y55y <y55y@users.noreply.github.com>
2026-04-30 03:46:09 +0000

    deploy: y55y/react-app-frontend@6e30fb1d5019bb78b0933f0d708e99725f19821d

---
+++ assets/skeleton-BK2I8i5m.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-D_OrFBAk.js";import{w as s}from"./index-ChE1QvV6.js";function r({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
--- assets/skeleton-DnpdxZE3.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./vendor-D_OrFBAk.js";import{x as s}from"./index-CjhFOErM.js";function r({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
