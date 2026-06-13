ed3d34ff0a5d6244573fb4d8ad80aea3bc684513
y55y <y55y@users.noreply.github.com>
2026-06-13 13:02:41 +0000

    deploy: y55y/react-app-frontend@17a19b4b819965859e0043ccbff6f0ed9ea4c576

---
+++ assets/skeleton-k25iWDnz.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-By0GTc2D.js";import{N as s}from"./index-DTfQQ3SE.js";function r({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
