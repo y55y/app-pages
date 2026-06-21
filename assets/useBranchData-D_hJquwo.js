de4332f45fdc70e927ae1d7291007db489d075e7
y55y <y55y@users.noreply.github.com>
2026-06-21 16:35:28 +0000

    deploy: middarcom/Frontend@270c0cf5c03cee48c6e2c634b8e66c72fc9a14c0

---
+++ assets/useBranchData-D_hJquwo.js
@@ -0,0 +1,1 @@
+import{aS as o}from"./index-lnMTvdN1.js";const c=()=>{const{selectedBranch:a,branches:n,changeBranch:r,isLoading:s,refreshBranches:h}=o();return{selectedBranch:a,branches:n,changeBranch:r,refreshBranches:h,isLoading:s,branchId:(a==null?void 0:a.BranchID)||null,Name:(a==null?void 0:a.Name)||null}};export{c as u};
