e10f0b3094964f3bcadddadc950b0b52cab003f7
y55y <y55y@users.noreply.github.com>
2026-07-02 06:20:55 +0000

    deploy: middarcom/Frontend@d653ed9b2c0cb166afaa4f3b585774374ff4b1f0

---
+++ assets/daily_entries.params-BQsYaLdr.js
@@ -0,0 +1,33 @@
+const n=`{
+  "journal_id": "JE-1001",
+  "journal_date": "2026-07-01",
+  "journal_reference": "REF-001",
+  "journal_description": "قيد يومية تجريبي",
+  "journal_currency_code": "SAR",
+  "journal_currency_rate": "1.00",
+  "journal_total_debit": "1,000.00",
+  "journal_total_credit": "1,000.00",
+  "journal_total_debit_without_symbol": "1,000.00",
+  "journal_total_credit_without_symbol": "1,000.00",
+  "business_name": "شركة تجريبية",
+  "site_bn1": "الفرع الرئيسي",
+  "site_address1": "الرياض",
+  "site_city": "الرياض",
+  "items": [
+    {
+      "account_code": "1010",
+      "account_name": "الصندوق",
+      "description": "طرف مدين",
+      "debit": "1,000.00",
+      "credit": ""
+    },
+    {
+      "account_code": "4010",
+      "account_name": "الإيرادات",
+      "description": "طرف دائن",
+      "debit": "",
+      "credit": "1,000.00"
+    }
+  ]
+}
+`;export{n as default};
