7ae2678eff39dd3e8329975276c4d66951ab1c42
y55y <y55y@users.noreply.github.com>
2026-07-02 12:26:47 +0000

    deploy: middarcom/Frontend@8fa351820d5c85e7e21a6c5440d7157aac0ea012

---
+++ assets/skeleton-D335dnD0.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-9UYLC0kE.js";import{N as s}from"./index-DLLAY4sV.js";function r({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
