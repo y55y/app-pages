18f75ab5bba36be6c1963550659fa2fa71c8f169
y55y <y55y@users.noreply.github.com>
2026-07-09 17:11:12 +0000

    deploy: middarcom/Frontend@202d61a5d57215308281bcda3a3cda629129c11d

--- assets/skeleton-DG_Ewf5I.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./vendor-DnwigAeT.js";import{X as s}from"./index-Ddl1NlHz.js";function r({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
