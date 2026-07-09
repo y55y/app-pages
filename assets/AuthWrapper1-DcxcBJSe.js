4a1415df0cf8b415015599c8c5e2b730ddc3174c
y55y <y55y@users.noreply.github.com>
2026-07-09 14:23:09 +0000

    deploy: middarcom/Frontend@4e0b333f2b1dd26cd9c414e644ede6107471d7b7

--- assets/AuthWrapper1-DcxcBJSe.js
+++
@@ -1,1 +0,0 @@
-import{i as o}from"./index-DNyJP3so.js";import{U as t}from"./vendor-BrLNotM4.js";const p=t("div")(({theme:e})=>({backgroundColor:e.palette.mode===o.DARK?e.palette.background.default:e.palette.grey[100],minHeight:"100vh"}));export{p as A};
--- assets/skeleton-CYc5flZ-.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./vendor-BrLNotM4.js";import{X as s}from"./index-DNyJP3so.js";function r({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
