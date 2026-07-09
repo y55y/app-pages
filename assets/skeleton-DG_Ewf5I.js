41af7874bb1353d2be3769e67daa38fdd769c848
y55y <y55y@users.noreply.github.com>
2026-07-09 17:07:54 +0000

    deploy: middarcom/Frontend@0a1a31db7478991901214f9d3eda5b2871fb7266

---
+++ assets/skeleton-DG_Ewf5I.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-DnwigAeT.js";import{X as s}from"./index-Ddl1NlHz.js";function r({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
---
+++ assets/textarea-BEYFG9ZQ.js
@@ -0,0 +1,16 @@
+import{j as a,X as n}from"./index-Ddl1NlHz.js";import{j as r}from"./vendor-DnwigAeT.js";/**
+ * @license lucide-react v0.543.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M11.562 3.266a.5.5 0 0 1 .876 0L15.39 8.87a1 1 0 0 0 1.516.294L21.183 5.5a.5.5 0 0 1 .798.519l-2.834 10.246a1 1 0 0 1-.956.734H5.81a1 1 0 0 1-.957-.734L2.02 6.02a.5.5 0 0 1 .798-.519l4.276 3.664a1 1 0 0 0 1.516-.294z",key:"1vdc57"}],["path",{d:"M5 21h14",key:"11awu3"}]],l=a("crown",o);/**
+ * @license lucide-react v0.543.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const i=[["path",{d:"M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7",key:"1m0v6g"}],["path",{d:"M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z",key:"ohrbg2"}]],u=a("square-pen",i);/**
+ * @license lucide-react v0.543.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=[["path",{d:"M16 17h6v-6",key:"t6n2it"}],["path",{d:"m22 17-8.5-8.5-5 5L2 7",key:"x473p"}]],p=a("trending-down",d);function x({className:e,...t}){return r.jsx("textarea",{"data-slot":"textarea",className:n("border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",e),...t})}export{l as C,u as S,x as T,p as a};
