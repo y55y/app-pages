c16760c11774e173703c7660c31492558058c64a
y55y <y55y@users.noreply.github.com>
2026-07-18 19:11:46 +0000

    deploy: middarcom/Frontend@cbc563342219bfeae7e3f6a906e1d34d419834c0

--- assets/AuthWrapper1-Dm2NJKAZ.js
+++
@@ -1,1 +0,0 @@
-import{i as o}from"./index-Rl3PydGQ.js";import{V as t}from"./vendor-H9TkgkWh.js";const p=t("div")(({theme:e})=>({backgroundColor:e.palette.mode===o.DARK?e.palette.background.default:e.palette.grey[100],minHeight:"100vh"}));export{p as A};
