d29b98073c830d811ef50d707393fcd8b222d877
y55y <y55y@users.noreply.github.com>
2026-07-18 19:41:50 +0000

    deploy: middarcom/Frontend@30512f7763006e7f05ce70f293fbfd5a808cb525

--- assets/AuthWrapper1-OxX2ltdn.js
+++
@@ -1,1 +0,0 @@
-import{i as o}from"./index-CFizvcpK.js";import{V as t}from"./vendor-3yYgD2tU.js";const p=t("div")(({theme:e})=>({backgroundColor:e.palette.mode===o.DARK?e.palette.background.default:e.palette.grey[100],minHeight:"100vh"}));export{p as A};
