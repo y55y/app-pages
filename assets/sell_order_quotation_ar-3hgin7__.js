0eebe463fad3db5a2290e5864cd04cd9dc0f3c73
y55y <y55y@users.noreply.github.com>
2026-07-27 09:56:40 +0000

    deploy: middarcom/Frontend@948a2e4820b8cf453bd3c00d5a7945da425ebd22

---
+++ assets/sell_order_quotation_ar-3hgin7__.js
@@ -0,0 +1,1 @@
+const t=""+new URL("sell_order_quotation_ar-DBqzP9jN.txt",import.meta.url).href;export{t as default};
--- assets/sell_order_quotation_ar-D_Kng8s5.js
+++
@@ -1,1 +0,0 @@
-const t=""+new URL("sell_order_quotation_ar-CFFsZsjU.txt",import.meta.url).href;export{t as default};
