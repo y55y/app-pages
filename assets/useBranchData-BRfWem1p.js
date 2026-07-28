812a5e8c7bea451ad818f79ee814fb1116c7769d
y55y <y55y@users.noreply.github.com>
2026-07-28 10:30:03 +0000

    deploy: middarcom/Frontend@4a244efdb244fccc40dcbb8372f10598395a3b5d

--- assets/useBranchData-BRfWem1p.js
+++
@@ -1,1 +0,0 @@
-import{a_ as o}from"./index-COrE_8Qp.js";const c=()=>{const{selectedBranch:a,branches:n,changeBranch:r,isLoading:s,refreshBranches:h}=o();return{selectedBranch:a,branches:n,changeBranch:r,refreshBranches:h,isLoading:s,branchId:(a==null?void 0:a.BranchID)||null,Name:(a==null?void 0:a.Name)||null}};export{c as u};
