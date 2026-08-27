f682d40c26a4da881f1e7e89cfc3a1bc6f27f5eb
y55y <y55y@users.noreply.github.com>
2026-08-27 16:24:09 +0000

    deploy: middarcom/Frontend@dd1f0f45f42aff9f88c661ee3de5ba2faf469c29

---
+++ assets/skeleton-DAuimhy-.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-pkAkFadU.js";import{$ as s}from"./index-CnHh0mtN.js";function r({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
--- assets/skeleton-H07mRkCT.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./vendor-pkAkFadU.js";import{$ as s}from"./index-DJWQ9mZe.js";function r({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
