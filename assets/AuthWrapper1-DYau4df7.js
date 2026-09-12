682a3da724557f8abbe3ef582ef60b4000db6dba
y55y <y55y@users.noreply.github.com>
2026-09-12 08:49:57 +0000

    deploy: middarcom/Frontend@73387c6afad5e343663199d972080743a66bf294

---
+++ assets/AuthWrapper1-DYau4df7.js
@@ -0,0 +1,1 @@
+import{i as o}from"./index-B3VBOZIr.js";import{V as t}from"./vendor-D-xq7IV-.js";const p=t("div")(({theme:e})=>({backgroundColor:e.palette.mode===o.DARK?e.palette.background.default:e.palette.grey[100],minHeight:"100vh"}));export{p as A};
