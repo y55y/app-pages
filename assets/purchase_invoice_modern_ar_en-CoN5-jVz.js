e9172a1e784807b0722e46a545affc864f38a166
y55y <y55y@users.noreply.github.com>
2026-09-12 17:16:50 +0000

    deploy: middarcom/Frontend@fec6cdbbff892b1c3577fd9b5f57d393269baff0

--- assets/purchase_invoice_modern_ar_en-CoN5-jVz.js
+++
@@ -1,1 +0,0 @@
-const e=""+new URL("purchase_invoice_modern_ar_en-OqCHb6Cj.txt",import.meta.url).href;export{e as default};
---
+++ assets/purchase_invoice_modern_ar_en-k7cR2-NV.js
@@ -0,0 +1,1 @@
+const e=""+new URL("purchase_invoice_modern_ar_en-DNC0cxGT.txt",import.meta.url).href;export{e as default};
